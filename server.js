41576d8adf821ab7ac1306167805c69950e393a4
Cadertea <cadertea@ncsu.edu>
2021-02-08 09:53:17 -0500

    changing server code

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+const compression = require("compression")
 
-const PORT = 3000;
+// require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-
-app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
